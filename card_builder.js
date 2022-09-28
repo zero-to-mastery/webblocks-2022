a271be9573d5e6accdc0b6253e3aced324a53138
MattCSmith <matt@matt-smith.dev>
2022-09-28 22:52:38 +0000

    Deploying to gh-pages from  @ c7ad250e6b0eb1a07b965a9dd38f9deb545e3bd6 🚀

---
+++ card_builder.js
@@ -0,0 +1,34 @@
+
+  // +--------------------------------------------------------------------------------+
+  // +                                                                                +
+  // +                 YOU DO NOT NEED TO CHANGE ANYTHING IN THIS FILE!               +
+  // +                                                                                +
+  // +--------------------------------------------------------------------------------+
+  
+  // Creates cards from the array above
+  // You don't need to modify this
+  let contents = [];
+  Shuffle(cards).forEach((c) => {
+    contents.push([
+      `<li class="card">` +
+        `<a href='${c.page_link}' target="_blank">` +
+        `<img class="art-image" src='${c.art_icon}' alt='${c.name}' onerror="this.src='https://cdn.discordapp.com/attachments/870676772683919390/891310666596630538/coming-soon-logo_okok.png';"/>` +
+        `</a>` +
+        `<div class="flex-content">` +
+        `<a href='${c.page_link}' target="_blank"><h3 class="art-title">${c.name}</h3></a>` +
+        `<p class='author'><a href="${c.author_github_url}" target="_blank"><i class="fab fa-github"></i> ${c.author_name}</a> </p>` +
+        `</div>` +
+        `</li>`
+    ]);
+  });
+  
+  document.getElementById('cards').innerHTML = contents.join('');
+  
+  function Shuffle(o) {
+    for (
+      var j, x, i = o.length;
+      i;
+      j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+    );
+    return o;
+  }
