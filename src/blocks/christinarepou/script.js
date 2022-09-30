619be3502381e50d836258958cafedf4af8d3a6c
Christina-Repou <chr.repou@gmail.com>
2022-10-01 00:42:36 +0200

    creating movie info web component

---
+++ src/blocks/christinarepou/script.js
@@ -0,0 +1,4 @@
+var myWebComponent = document.getElementById("my-web-component");
+var tempContent = myWebComponent.content;
+var tempDiv = document.getElementById('temp-div');
+tempDiv.appendChild(tempContent);
