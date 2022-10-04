fc1c51573f6a12844181a0c3cacbad4ec034e6c9
blitzmartin <lizmartinmusic@gmail.com>
2022-10-04 15:13:48 +0200

    first version done,missing goto next when finished

--- src/blocks/blitzmartin/data.js
+++ src/blocks/blitzmartin/data.js
@@ -2,16 +2,74 @@ const songs = [
   {
     id: 0,
     songTitle: "Misty",
-    artist: "Blitzmartin",
+    artist: "BLiTZ",
     source: "./assets/music/Liz Martin_Misty_LoFi.mp3",
-    cover:
-      "https://www.japannakama.co.uk/wp-content/uploads/2021/10/lo-fi-girl-feature.jpg",
+    cover: "https://i.scdn.co/image/ab67616d0000b2735419c0986ee778b7489da418",
   },
   {
     id: 1,
-    songTitle: "LoFi Tokyo",
-    artist: "Masahiro",
-    source: "./assets/music/Liz Martin_Misty_LoFi.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b27328bde58a46b5b0b668fb7d61",
+    songTitle: "City Lights",
+    artist: "Liz Martin",
+    source: "./assets/music/Liz Martin - City Lights.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b27355d36ed52603f86adce47044",
+  },
+  {
+    id: 2,
+    songTitle: "LoFi X-Mas",
+    artist: "BLiTZ",
+    source: "./assets/music/Liz Martin_LoFi Xmas.wav",
+    cover: "https://i1.sndcdn.com/artworks-000658386451-100pg1-t500x500.jpg",
+  },
+  {
+    id: 3,
+    songTitle: "Nyctalopia",
+    artist: "Liz Martin X v a s s h",
+    source: "./assets/music/Liz Martin - nyctalopia (prod. v a s s h).mp3",
+    cover: "https://i1.sndcdn.com/artworks-000097315214-xuheja-t500x500.jpg",
+  },
+  {
+    id: 4,
+    songTitle: "Mango",
+    artist: "Liz Martin",
+    source: "./assets/music/Liz Martin - Mango.mp3",
+    cover:
+      "https://cdns-images.dzcdn.net/images/cover/d4daf9009f14f4dbdf04635abaac5ee1/264x264.jpg",
+  },
+  {
+    id: 5,
+    songTitle: "Bang Bang",
+    artist: "Liz Martin",
+    source: "./assets/music/Liz Martin_Bang Bang.mp3",
+    cover: "https://i1.sndcdn.com/artworks-000104202518-pogdts-t240x240.jpg",
+  },
+
+  {
+    id: 6,
+    songTitle: "Immaculate Heart",
+    artist: "BLiTZ",
+    source: "./assets/music/Liz Martin_The Immaculate Heart.mp3",
+    cover: "https://i1.sndcdn.com/artworks-000588334994-2wrysh-t500x500.jpg",
+  },
+  {
+    id: 7,
+    songTitle: "La Moda Italiana",
+    artist: "BLiTZ",
+    source: "./assets/music/Liz Martin_Schubert_MilanoFashionWeek.mp3",
+    cover: "https://i1.sndcdn.com/artworks-000125251010-j2u4en-t500x500.jpg",
+  },
+  {
+    id: 8,
+    songTitle: "Prisoner",
+    artist: "Liz Martin (ft Singrey)",
+    source: "./assets/music/Liz Martin_Prisoner.mp3",
+    cover:
+      "https://i1.sndcdn.com/artworks-DFX2yzNZp8t24L1K-RbWvSA-t500x500.jpg",
+  },
+  {
+    id: 9,
+    songTitle: "Dua Lipa x Daft Punk",
+    artist: "Liz Martin",
+    source: "./assets/music/Liz Martin_DON'T START NOW_GET LUCKY.mp3",
+    cover: "https://i1.sndcdn.com/artworks-000637024867-m6jwy6-t500x500.jpg",
   },
 ];
