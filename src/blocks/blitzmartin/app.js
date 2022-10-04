997ced2e12cf6effb03b5a1aa6d45bb17ac147fd
blitzmartin <lizmartinmusic@gmail.com>
2022-10-04 11:48:06 +0200

    play/pause working,added correct duration

--- src/blocks/blitzmartin/app.js
+++ src/blocks/blitzmartin/app.js
@@ -2,8 +2,9 @@ let currentMusic = 0; //index of songs array for current music playing
 
  //point to element in DOM
 const music = document.querySelector('#audio');
-const song = document.querySelector('.song');
+const songTitle = document.querySelector('.song-title');
 const artist = document.querySelector('.artist');
+const timeBar = document.querySelector('.time-bar');
 const currentTime = document.querySelector('.current-time');
 const songDuration = document.querySelector('.song-duration');
 const playPauseBtn = document.querySelector('.play-pause-btn');
@@ -15,7 +16,41 @@ const skipNextBtn = document.querySelector('.next');
 playPauseBtn.addEventListener('click', () => {
   if (playPauseSpan.innerHTML === "play_circle") {
     playPauseSpan.innerHTML = "pause_circle";
+    music.play();
   } else {
     playPauseSpan.innerHTML = "play_circle";
+    music.pause();
   }
-})
+});
+
+const setMusic = (i) => {
+    timeBar.value = 0;
+    let song = songs[i];
+    currentMusic = i;
+    music.src = song.source;
+    songTitle.innerHTML = song.songTitle;
+    artist.innerHTML = song.artist;
+
+    currentTime.innerHTML = '00:00'
+    //setTimeout is needed to slightly delay accessing to music since it takes a little while to load it
+    setTimeout(() => {
+        timeBar.max = music.songDuration;
+        songDuration.innerHTML = formatTime(music.duration);
+        console.log(music.songDuration);
+    }, 300)
+
+}
+
+setMusic(0);
+
+const formatTime = (time) => {
+    let minutes = Math.floor(time / 60)
+    if ( minutes < 10 ) {
+        minutes = `0${minutes}`;
+    }
+    let seconds = Math.floor(time % 60)
+    if ( seconds < 10 ) {
+        seconds = `0${minutes}`;
+    }
+    return `${minutes} : ${seconds}`;
+}
--- src/blocks/blitzmartin/data.js
+++ src/blocks/blitzmartin/data.js
@@ -1,17 +1,17 @@
 const songs = [
   {
-    id: 1,
-    song: "Misty",
+    id: 0,
+    songTitle: "Misty",
     artist: "Liz Martin",
     source: "./assets/music/Liz Martin_Misty_LoFi.mp3",
     cover:
       "https://www.japannakama.co.uk/wp-content/uploads/2021/10/lo-fi-girl-feature.jpg",
   },
   {
-    id: 2,
-    song: "",
-    artist: "",
-    source: "",
-    cover: "",
+    id: 1,
+    songTitle: "LoFi Tokyo",
+    artist: "Masahiro",
+    source: "./assets/music/Liz Martin_Misty_LoFi.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b27328bde58a46b5b0b668fb7d61",
   },
 ];
