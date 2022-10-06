053c494f4266dd229efac5318cccf848d3a879f6
daytonschuh <daytonschuh@gmail.com>
2022-10-06 08:40:33 -0700

    Learning about promises

--- src/blocks/daytonschuh/script.js
+++ src/blocks/daytonschuh/script.js
@@ -9,16 +9,38 @@
         Unveil pokemon
 */
 
-// base url
+// API base url
 let baseApiUrl = "https://pokeapi.co/api/v2/";
 let id = 5;
 // endpoint
 let endpoint = `pokemon/${id}`;
-// printing pending promise, but contains expected details -- need to explore promises a bit
-console.log(fetch(baseApiUrl + endpoint).then(response => response.json()));
+
+function getPokemon()
+{
+    // TODO
+    id = getRandomIntInclusive(1, 151); // first gen
+    let response = fetch(baseApiUrl + endpoint).then(response => response.json());
+    let pokemon = response.results; // result undefined, but base promise is pending
+    console.log(pokemon);
+}
+
+function checkGuess()
+{
+    // TODO
+}
+
+// Following code found at: https://developer.mozilla.org/en-US/docs/web/javascript/reference/global_objects/math/random
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  }
+
+getPokemon();
 
 /*
     Future considerations:
         "Who's that pokemon?" audio cue / Background music
         Scoring (maybe based on time taken to guess)
+        Select different generations
 */
