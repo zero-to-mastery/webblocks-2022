b2d4320cdedceff8cc2e1476907705b5b63d2c72
daytonschuh <daytonschuh@gmail.com>
2022-10-06 09:22:17 -0700

    Check user guess

--- src/blocks/daytonschuh/script.js
+++ src/blocks/daytonschuh/script.js
@@ -1,17 +1,25 @@
 /*
     Base functionality:
-        Make a call to pokeapi
-        Get random pokemon
+        Make a call to pokeapi ✓
+        Get random pokemon ✓
         Display blacked out image of pokemon
         Wait for user
-        Get guess from user
-        Check if guess is correct
+        Get guess from user ✓
+        Check if guess is correct ✓
         Unveil pokemon
 */
 
 // API base url
 let baseApiUrl = "https://pokeapi.co/api/v2/";
 let id = 5;
+let pokemon = "";
+const playerGuess = document.getElementById('playerGuess');
+
+playerGuess.addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+        checkGuess();
+    }
+});
 
 async function callPokeAPI(){
     let endpoint = `pokemon/${id}`
@@ -23,13 +31,22 @@ async function getPokemon()
 {
     // TODO
     id = getRandomIntInclusive(1, 151); // first gen
-    let pokemon = await callPokeAPI();
+    pokemon = await callPokeAPI();
     console.log(pokemon.name);
     //setTimeout(()=> getPokemon(), 5000);
 }
 
 function checkGuess()
 {
+    // TODO
+    if(playerGuess.value.toLowerCase() === pokemon.name.toLowerCase()){
+        console.log("Good job!");
+    }
+    playerGuess.value = "";
+    revealPokemon();
+}
+
+function revealPokemon(){
     // TODO
 }
 
