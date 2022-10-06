bd71c971ceda86b6699ddd8ee3c9d4d74531cafd
daytonschuh <daytonschuh@gmail.com>
2022-10-06 08:03:52 -0700

    Building blocks

---
+++ src/blocks/daytonschuh/script.js
@@ -0,0 +1,16 @@
+/*
+    Base functionality:
+        Make a call to pokeapi
+        Get random pokemon
+        Display blacked out image of pokemon
+        Wait for user
+        Get guess from user
+        Check if guess is correct
+        Unveil pokemon
+*/
+
+/*
+    Future considerations:
+        "Who's that pokemon?" audio cue / Background music
+        Scoring (maybe based on time taken to guess)
+*/
