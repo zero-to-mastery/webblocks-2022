06fd9596356c9221ac7e4a984ad6db6c5a2bc217
Md-Aquib <aquibali1209@gmail.com>
2022-10-11 19:06:42 +0530

    'Fourth-Pull_request'

---
+++ src/blocks/Md-Aquib/contact.js
@@ -0,0 +1,4 @@
+function submitted() {
+    var name = document.getElementById('name').value;
+    alert("Hello "+name+" A Mail Window will soon open on your computer click on send to send your message.");
+}
