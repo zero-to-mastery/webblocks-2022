9afa28996409fe634933de371d67e4147435f245
sophiabrandt <16630701+sophiabrandt@users.noreply.github.com>
2022-10-13 19:34:51 +0000

    Deploying to gh-pages from  @ f6e06e0a0b72c028d15fb16aa1ba99b7fd3f4e91 🚀

---
+++ blocks/cquaid_2/script.js
@@ -0,0 +1,8 @@
+ let modal = document.getElementsByClassName("modal")[0];
+    function showmodal() {
+      modal.style.bottom = "40%";
+      modal.style.left = "35%";
+    }
+    function hidemodal() {
+      modal.style.bottom = "-300px";
+    }
