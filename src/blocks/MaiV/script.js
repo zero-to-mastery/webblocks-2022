a5fbb7c96743421b768bd711dccaf80012185591
Mai Vang <vmaineng@gmail.com>
2022-10-13 04:34:52 -0700

    added time

--- src/blocks/MaiV/script.js
+++ src/blocks/MaiV/script.js
@@ -1,1 +1,12 @@
 //play
+
+const music = document.querySelector("#audio");
+const play = document.querySelector("#play");
+const rewind = document.querySelector("#rewind");
+const forward = document.querySelector("#forward");
+
+play.addEventListener("click", () => {
+    if (play.innerHTML === "play_circle") {
+        play
+    }
+})
---
+++ src/blocks/MaiV/songs.js
@@ -0,0 +1,10 @@
+const songs = [
+    {
+        id: 0,
+        songTitle: "Studying on the balcony 🌜 Relaxing music for stress relief - Music to put you in a better mood",
+        artist: "BLiTZ",
+        source: "https://www.youtube.com/watch?v=kxBr9TbrBSo",
+        cover: "https://i.scdn.co/image/ab67616d0000b2735419c0986ee778b7489da418",
+    }
+]
+
