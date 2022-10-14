490efef001dd4fd9cc0fb5c6ba8b58a24c960671
MattCSmith <matt@matt-smith.dev>
2022-10-14 01:54:51 +0000

    Deploying to gh-pages from  @ c5cb454252549a3db78e6f3174185ba38c51145b 🚀

--- blocks.js
+++ blocks.js
@@ -483,6 +483,13 @@
     "author_github_url": "https://github.com/corquaid/"
   },
   {
+    "name": "Button with Popup Modal",
+    "page_link": "./blocks/cquaid_2/index.html",
+    "block_icon": "./blocks/cquaid_2/icon.png",
+    "author_name": "Cormac Quaid",
+    "author_github_url": "https://github.com/corquaid/"
+  },
+  {
     "name": "Timeline",
     "page_link": "./blocks/danicc/index.html",
     "block_icon": "./blocks/danicc/icon.png",
