013f35d6fdc97e3e336f25b0a9991be8d8cdb317
blitzmartin <lizmartinmusic@gmail.com>
2022-10-20 15:12:24 +0200

    added selectors and event listeners

--- src/blocks/blitzmartin-4/app.js
+++ src/blocks/blitzmartin-4/app.js
@@ -1,3 +1,28 @@
-const translateBtn = document.querySelector('#translate-btn');
+const translateBtn = document.querySelector("#translate-btn");
+const textarea = document.querySelector("textarea");
+const langSelection = document.querySelector("select");
+
+translateBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  if( textarea.value !== '') {
+    textToSpeech(textarea.value);
+  }
+});
+
+speechSynthesis.addEventListener('voiceschanged', changeLang)
+
+/* langSelectio.addEventListener('click', (e) => {
+ changeLang(lang.value);   
+}) */
 
 
+const textToSpeech = (text) => {
+    let utterance = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(utterance);
+}
+
+const changeLang = (option) => {
+    for (let lang of speechSynthesis.getVoices()) {
+
+    }
+}
