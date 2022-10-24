0633e4be52f48f53860771c66abad0c51bdf1d9b
Elisa <95719665+EliSacch@users.noreply.github.com>
2022-10-24 19:32:34 +0100

    Add functions to change active element on click

--- src/blocks/EliSacch/script.js
+++ src/blocks/EliSacch/script.js
@@ -1,6 +1,19 @@
-/* Invert x-index for cards after active */
-
+/* When content loads */
 document.addEventListener("DOMContentLoaded",function() {
+    
+    /* Add event listener to cards */
+    const cards = document.getElementsByClassName('card');
+    for(let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener('click', makeActive);
+    }
+
+    pushBack();
+})
+
+/**
+ * This function inverts z-index for cards after active
+ */
+function pushBack() {
     let cardsAfter = document.getElementsByClassName('after');
     if(cardsAfter != null) {
         zIndex = -1;
@@ -8,6 +21,26 @@ document.addEventListener("DOMContentLoaded",function() {
             cardsAfter[z].style.zIndex = zIndex;
             zIndex--;
         } 
-    }    
-})
+    } 
+}
+
+/**
+ * This function makes the clicked card active
+ */
+function makeActive() {
+    const cards = document.getElementsByClassName('card');
+    for(let i = 0; i < cards.length; i++) {
+        cards[i].classList.remove('active-card');
+        cards[i].classList.remove('after');
+        cards[i].style.zIndex = 0;
+    }
+    this.classList.add('active-card');
+    
+    let position = this.getAttribute('data-position');
+    for(let i = position; i < cards.length; i++) {
+        cards[i].classList.add('after');
+    }
+    pushBack();
+}
+
 
