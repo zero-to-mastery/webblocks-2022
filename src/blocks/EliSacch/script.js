7c594ae2d9cc82a65a8107c620de7918fa4edc5d
Elisa <95719665+EliSacch@users.noreply.github.com>
2022-10-24 21:05:43 +0100

    Add function to center active element and space out the others

--- src/blocks/EliSacch/script.js
+++ src/blocks/EliSacch/script.js
@@ -8,21 +8,8 @@ document.addEventListener("DOMContentLoaded",function() {
     }
 
     pushBack();
+    spaceCards(3);
 })
-
-/**
- * This function inverts z-index for cards after active
- */
-function pushBack() {
-    let cardsAfter = document.getElementsByClassName('after');
-    if(cardsAfter != null) {
-        zIndex = -1;
-        for(let z = 0; z < cardsAfter.length; z++) {
-            cardsAfter[z].style.zIndex = zIndex;
-            zIndex--;
-        } 
-    } 
-}
 
 /**
  * This function makes the clicked card active
@@ -33,14 +20,45 @@ function makeActive() {
         cards[i].classList.remove('active-card');
         cards[i].classList.remove('after');
         cards[i].style.zIndex = 0;
+        cards[i].style.translate = "-50%";
     }
     this.classList.add('active-card');
     
-    let position = this.getAttribute('data-position');
+    const position = this.getAttribute('data-position');
     for(let i = position; i < cards.length; i++) {
         cards[i].classList.add('after');
     }
     pushBack();
+    spaceCards(position-1);
 }
 
+/**
+ * This function inverts z-index for cards after active
+ */
+ function pushBack() {
+    const cardsAfter = document.getElementsByClassName('after');
+    if(cardsAfter != null) {
+        zIndex = -1;
+        for(let z = 0; z < cardsAfter.length; z++) {
+            cardsAfter[z].style.zIndex = zIndex;
+            zIndex--;
+        } 
+    } 
+}
 
+/**
+ * This function changes the horizontal position of the cards
+ */
+ function spaceCards(position) {
+    const cards = document.getElementsByClassName('card');
+    let moveLeft = -490;
+    for(let i = position-1; i >= 0; i--) {
+        cards[i].style.translate = `${moveLeft}px`;
+        moveLeft -= 150;
+    }
+    let moveRight = 0;
+    for(let i = position+1; i < cards.length; i++) {
+        cards[i].style.translate = `${moveRight }px`;
+        moveRight += 150;
+    } 
+}
