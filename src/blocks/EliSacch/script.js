b018c36d84c98648bf5e0d66188a9999d9789faa
Elisa <95719665+EliSacch@users.noreply.github.com>
2022-10-24 18:48:23 +0100

    Add images

--- src/blocks/EliSacch/script.js
+++ src/blocks/EliSacch/script.js
@@ -1,8 +1,13 @@
+/* Invert x-index for cards after active */
+
 document.addEventListener("DOMContentLoaded",function() {
     let cardsAfter = document.getElementsByClassName('after');
-    zIndex = -1;
-    for(let z = 0; z < cardsAfter.length; z++) {
-        cardsAfter[z].style.zIndex = zIndex;
-        zIndex--;
-    }   
+    if(cardsAfter != null) {
+        zIndex = -1;
+        for(let z = 0; z < cardsAfter.length; z++) {
+            cardsAfter[z].style.zIndex = zIndex;
+            zIndex--;
+        } 
+    }    
 })
+
