110cc5b7a7e06bd981871596f09654d8f844853f
teri eyenike <teyenike1@gmail.com>
2022-11-01 03:23:42 +0100

    Add: javascript interactivity to faq

---
+++ src/blocks/teri_3/app.js
@@ -0,0 +1,29 @@
+const scrollLine = document.querySelector('.scroll-line');
+
+function fillScrollLine() {
+  const windowHeight = window.innerHeight;
+  const fullHeight = document.body.clientHeight;
+  const scrolled = window.scrollY;
+  const percentScrolled = (scrolled / (fullHeight - windowHeight)) * 100;
+
+  scrollLine.style.width = `${percentScrolled}%`;
+}
+
+window.addEventListener('scroll', debounce(fillScrollLine));
+
+function debounce(func, wait = 15, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
